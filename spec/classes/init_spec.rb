dac6089362228ca03db5c8923da3e4425ceeb7a7
othalla <othalla.lf@gmail.com>
2018-12-03 22:07:04 +0100

    Add compile test with default parameters

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 describe 'tautulli' do
   context 'with default values for all parameters' do
+    it { is_expected.to compile.with_all_deps }
     it { is_expected.to contain_class('tautulli') }
   end
 end
